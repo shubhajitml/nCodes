98e82419d45a888a2db7cc3bb08ce47ecfcc9df4
Shubhajitdas121 <shubhajitdas121@gmail.com>
2019-01-06 23:58:10 +0530

    lin_reg

---
+++ pytorch-examples/01-basics/2. linear_regression/lin_reg.py
@@ -0,0 +1,23 @@
+# Code referenced from https://github.com/yunjey/pytorch-tutorial/tree/master/tutorials/01-basics/linear_regression/main.py
+# (not the copy & paste version!)
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Hyper-parameters
+input_size = 1
+output_size = 1
+num_epochs = 60
+learning_rate = 0.001
+
+# Toy dataset
+x_train = np.array([[3.3], [4.4], [5.5], [6.71], [6.93], [4.168], 
+                    [9.779], [6.182], [7.59], [2.167], [7.042], 
+                    [10.791], [5.313], [7.997], [3.1]], dtype=np.float32)
+
+y_train = np.array([[1.7], [2.76], [2.09], [3.19], [1.694], [1.573], 
+                    [3.366], [2.596], [2.53], [1.221], [2.827], 
+                    [3.465], [1.65], [2.904], [1.3]], dtype=np.float32)
+
