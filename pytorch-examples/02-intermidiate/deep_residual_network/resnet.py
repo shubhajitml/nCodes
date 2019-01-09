b7af5b72ec41b98d12e29d3a4634f6e8293f468f
Shubhajitdas121 <shubhajitdas121@gmail.com>
2019-01-09 23:30:34 +0530

    residual block

--- pytorch-examples/02-intermidiate/deep_residual_network/resnet.py
+++ pytorch-examples/02-intermidiate/deep_residual_network/resnet.py
@@ -50,7 +50,28 @@ def conv3x3(in_channels, out_channels, stride=1):
                      stride=stride, padding=1, bias=False)
 
 # Residual block
-
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+        
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
 # Resnet
 
 # Loss and optimizer
